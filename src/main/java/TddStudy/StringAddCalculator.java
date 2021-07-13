01a6140474e90ed5fb3f7b8a786f8c98d71761cd
wkd-woo <wkdwoos@gmail.com>
2021-07-13 16:33:58 +0900

    feat (StringAddCalculator) : 쉼표 or 콜론 구분자
    쉼표, 콜론 구분자 적용
    
    + 이후 커스텀 구분자 적용
    + 리팩토링 필요
    + 음수 전달시 예외 발생

--- src/main/java/TddStudy/StringAddCalculator.java
+++ src/main/java/TddStudy/StringAddCalculator.java
@@ -4,9 +4,22 @@ public class StringAddCalculator {
 
 
     public static int splitAndSum(String text) {
+
         if (text == null || text.isEmpty())
             return 0;
 
+        if (text.length() == 1)
+            return Integer.parseInt(text);
+
+        if (text.contains(",") || text.contains(";") || text.contains(":")) {
+            int result = 0;
+            String[] texts = text.split(",|:|;");
+
+            for (String digit : texts) {
+                result += Integer.parseInt(digit);
+            }
+            return result;
+        }
 
         return 1;
     }
